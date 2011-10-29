d2775a1402b460fd8cc3958e370320923130b794
Vincent <phenolplus@hotmail.com>
2011-10-29 11:48:26 +0800

    Added all files in my project

---
+++ src/test/sensors/SensorCheckActivity.java
@@ -0,0 +1,135 @@
+package test.sensors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SensorCheckActivity extends Activity {
+    /** Members Declaration */
+	private SensorManager manager;
+	private Sensor sensor,compass;
+	private TextView accInfo,readX,readY,readZ,comInfo,compX,compY,compZ;
+	private SensorEventListener aListener,mListener;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        accInfo = (TextView) findViewById(R.id.textView1);
+        readX = (TextView) findViewById(R.id.textViewX);
+        readY = (TextView) findViewById(R.id.textViewY);
+        readZ = (TextView) findViewById(R.id.textViewZ);
+        
+        comInfo = (TextView) findViewById(R.id.textView2);
+        compX = (TextView) findViewById(R.id.textViewP);
+        compY = (TextView) findViewById(R.id.textViewQ);
+        compZ = (TextView) findViewById(R.id.textViewR);
+        
+        // Get sensors
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        // Get accelerometer
+        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // Get compass
+        compass = manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    	
+        // setup accelerometer       
+        if(sensor == null) {
+        	// not found
+        	Log.e("Sensors","sensor NOT found");
+        	accInfo.setText("No Acelerometer found !!!");
+        	Toast.makeText(this, "Error occured !!", Toast.LENGTH_SHORT).show();
+        	readX.setText("Sorry ...");
+        	readY.setText("Sorry ...");
+        	readZ.setText("Sorry ...");
+        } else {
+        	// Print informations
+        	String str ="\nAccelerometer is :\n"
+        			+ "Sensor Type = " + sensor.getType() + "\n"
+        			+ "Sensor made by :" + sensor.getVendor() + "\n" 
+        			+ "Time resolution = " + sensor.getMinDelay() + "us\n"
+        			+ "Resolution = " +sensor.getResolution() + "\n";
+        	accInfo.setText(str);
+        	setAccelerometerListener();
+        	Log.e("Sensors","Accelerometer set !");
+        }
+        
+        // setup compass
+        if(compass == null){
+        	// not found
+        	Log.e("Sensors","sensor NOT found");
+        	comInfo.setText("No Magnetic-field sensor found !!!");
+        	Toast.makeText(this, "Error occured !!", Toast.LENGTH_SHORT).show();
+        	compX.setText("Sorry ...");
+        	compY.setText("Sorry ...");
+        	compZ.setText("Sorry ...");
+        } else {
+            	// Print informations
+            	String str ="\n\nMagnetic Field Sensor is :\n"
+            			+ "Sensor Type = " + compass.getType() + "\n"
+            			+ "Sensor made by :" + compass.getVendor() + "\n" 
+            			+ "Time resolution = " + compass.getMinDelay() + "us\n"
+            			+ "Resolution = " +compass.getResolution() + "\n";
+            	comInfo.setText(str);
+            	setCompassListener();
+            	Log.e("Sensors","Compass set !");
+        }
+        
+        
+        Log.e("Application","Sensor Activity Created");
+    }
+    
+    private void setAccelerometerListener(){
+    	// setup & implement accelerometer listener
+    	aListener = new SensorEventListener(){
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				// TODO Auto-generated method stub
+				readX.setText(String.format("read X = %+.3f m/s^2",event.values[0]));
+				readY.setText(String.format("read Y = %+.3f m/s^2",event.values[1]));
+				readZ.setText(String.format("read Z = %+.3f m/s^2",event.values[2]));
+			}
+    		
+    		
+    	};
+    	manager.registerListener(aListener, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+    
+    private void setCompassListener(){
+    	mListener = new SensorEventListener(){
+
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				// TODO Auto-generated method stub
+				compX.setText(String.format("read X = %+.3f uT",event.values[0]));
+				compY.setText(String.format("read Y = %+.3f uT",event.values[1]));
+				compZ.setText(String.format("read Z = %+.3f uT",event.values[2]));
+			}
+    		
+    	};
+    	manager.registerListener(mListener, compass, SensorManager.SENSOR_DELAY_GAME);
+    }
+    
+    
+}
